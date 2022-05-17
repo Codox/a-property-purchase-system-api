db174a0dca2b2a486b7bf2f9fa97afd99120175f
Connor Brady <connorbrady@codox.eu>
2022-05-17 23:01:36 +0100

    ADDED User model

---
+++ src/main/java/uk/codox/apps/models/User.java
@@ -0,0 +1,31 @@
+package uk.codox.apps.models;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(unique = true)
+    private UUID uuid;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
