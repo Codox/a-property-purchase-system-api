dfffa6aeea11e57102cb9a59445f4fd809abe6b0
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-05-17 22:29:52 +0100

    Initial commit

---
+++ src/test/java/uk/codox/apps/AppsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.codox.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
