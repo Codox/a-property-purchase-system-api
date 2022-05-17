e73d0ebf85fac668ff93b38b8faf9c24a16a9ecc
Connor Brady <connorbrady@codox.eu>
2022-05-17 23:14:41 +0100

    ADDED Get user endpoint for testing

--- src/main/java/uk/codox/apps/controllers/UserController.java
+++ src/main/java/uk/codox/apps/controllers/UserController.java
@@ -1,9 +1,22 @@
 package uk.codox.apps.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.codox.apps.models.User;
+import uk.codox.apps.repository.UserRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getUser() {
+        return (List<User>) this.userRepository.findAll();
+    }
 }
